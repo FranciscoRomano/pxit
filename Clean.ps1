cb1c7fd3b2bd6b6ac7e18fb80af8b9d25bf19fef
Francisco Romano <58254306+FranciscoRomano@users.noreply.github.com>
2024-06-27 18:26:07 +1000

    fixing compiler errors

---
+++ Clean.ps1
@@ -0,0 +1,6 @@
+git clean -dfX
+$tdc="$PSScriptRoot"
+do {
+  $dirs = gci $tdc -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+  $dirs | Foreach-Object { Remove-Item $_ }
+} while ($dirs.count -gt 0)
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <OpenGL_WGL.h>
 #include <iostream>
 #include <string>
+#include <vector>
 #include <Windows.h>
 #include <Macros/Architecture.h>
 #include <Macros/Compiler.h>
