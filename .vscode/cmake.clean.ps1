a45b0491c99ecf5c21c603a8ae3d1a76d8f27bdb
Francisco Romano <58254306+FranciscoRomano@users.noreply.github.com>
2024-11-10 20:20:58 +1100

    removed old clean and build scripts in favour or cmake extension

--- .vscode/cmake.clean.ps1
+++
@@ -1,6 +0,0 @@
-git clean -dfX
-$tdc="$PSScriptRoot/../"
-do {
-  $dirs = gci $tdc -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
-  $dirs | Foreach-Object { Remove-Item $_ }
-} while ($dirs.count -gt 0)
