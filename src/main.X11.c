76ae70962eaca4660967caba3b69e7b5586c68f8
Francisco Romano <fcalaisromano@gmail.com>
2024-11-16 21:08:33 +1100

    updating x11 backup scenario

--- src/main.X11.c
+++ src/main.X11.c
@@ -1,64 +1,54 @@
-#include "platform/X11/context.h"
+#include "platform/X11/library.h"
+#include "platform/X11/window.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-extern PxitPlatformX11 GX11;
+extern LibraryX11 X11;
 
 int main(int argc, char** argv)
 {
-    InitPxitPlatformX11();
+    if (!LoadLibraryX11())
+    {
+        printf("ERROR: failed to load X11 library\n");
+        exit(EXIT_FAILURE);
+    }
 
-    Atom wmDeleteMessage = GX11.XInternAtom(GX11.dpy, "WM_DELETE_WINDOW", False);
+    WindowContextX11 ctx;
+    if (!CreateWindowContextX11(&ctx))
+    {
+        printf("ERROR: failed to create X11 window context\n");
+        exit(EXIT_FAILURE);
+    }
 
-    XSetWindowAttributes attr = {};
-    attr.background_pixel = 0xffafe9af;
-    attr.event_mask = StructureNotifyMask | KeyPressMask | KeyReleaseMask | ExposureMask;
-
-    Window win = GX11.XCreateWindow(
-        GX11.dpy,
-        GX11.XDefaultRootWindow(GX11.dpy),
-        0,
-        0,
-        800,
-        600,
-        0,                         // border width
-        CopyFromParent,            // window depth
-        CopyFromParent,            // window class
-        CopyFromParent,            // window visual
-        CWBackPixel | CWEventMask, // attribute mask
-        &attr
-    );
-    GX11.XMapWindow(GX11.dpy, win);
-
-    GX11.XSetWMProtocols(GX11.dpy, win, &wmDeleteMessage, 1);
+    WindowX11 win;
+    if (!CreateWindowX11(&ctx, 800, 600, &win))
+    {
+        printf("ERROR: failed to create X11 window\n");
+        exit(EXIT_FAILURE);
+    }
 
     int is_window_open = 1;
     while (is_window_open)
     {
         XEvent evt = {};
-        GX11.XNextEvent(GX11.dpy, &evt);
+        X11.XNextEvent(ctx.hDisplay, &evt);
 
         switch(evt.type)
         {
             case KeyPress:
             case KeyRelease:
-            {
-                XKeyPressedEvent *Event = (XKeyPressedEvent*)&evt;
-                if(Event->keycode == GX11.XKeysymToKeycode(GX11.dpy, XK_Escape))
-                {
+                if (evt.xkey.keycode == X11.XKeysymToKeycode(ctx.hDisplay, XK_Escape))
                     is_window_open = 0;
-                }
                 break;
-            }
             case ClientMessage:
-            {
-                XClientMessageEvent* cm_evt = (XClientMessageEvent*)(&evt);
-                if (evt.xclient.data.l[0] == wmDeleteMessage)
+                if (evt.xclient.window == win.hID && evt.xclient.data.l[0] == ctx.wmDeleteWindow)
                     is_window_open = 0;
-            }
+                break;
         }
     }
 
-    FreePxitPlatformX11();
+    DestroyWindow(&ctx, &win);
+    DestroyWindowContextX11(&ctx);
+    FreeLibraryX11();
     return 0;
 }
