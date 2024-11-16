99ac22257629b0a5e236bdad0555eaefbe6272b4
Francisco Romano <fcalaisromano@gmail.com>
2024-11-17 00:30:13 +1100

    experienting with X11 read events function

--- src/main.c
+++ src/main.c
@@ -5,6 +5,37 @@
 
 extern LibraryX11 X11;
 
+int is_window_open = 1;
+
+bool ReadWindowEventsX11(WindowContextX11* context)
+{
+    XEvent event;
+    WindowX11* window = NULL;
+
+    X11.XPending(context->hDisplay);
+
+    while (QLength(context->hDisplay))
+    {
+        X11.XNextEvent(context->hDisplay, &event);
+
+        if (X11.XFindContext(context->hDisplay, event.xany.window, context->hContext, (XPointer*)&window)) continue;
+
+        switch (event.type)
+        {
+            case KeyRelease:
+                if (event.xkey.keycode == X11.XKeysymToKeycode(context->hDisplay, XK_Escape))
+                    is_window_open = 0;
+                break;
+            case ClientMessage:
+                if (event.xclient.data.l[0] == context->wmDeleteWindow)
+                    is_window_open = 0;
+                break;
+        }
+    }
+
+    X11.XFlush(context->hDisplay);
+}
+
 int main(int argc, char** argv)
 {
     if (!LoadLibraryX11())
@@ -27,24 +58,9 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    int is_window_open = 1;
     while (is_window_open)
     {
-        XEvent evt = {};
-        X11.XNextEvent(ctx.hDisplay, &evt);
-
-        switch(evt.type)
-        {
-            case KeyPress:
-            case KeyRelease:
-                if (evt.xkey.keycode == X11.XKeysymToKeycode(ctx.hDisplay, XK_Escape))
-                    is_window_open = 0;
-                break;
-            case ClientMessage:
-                if (evt.xclient.window == win.hID && evt.xclient.data.l[0] == ctx.wmDeleteWindow)
-                    is_window_open = 0;
-                break;
-        }
+        ReadWindowEventsX11(&ctx);
     }
 
     DestroyWindow(&ctx, &win);
