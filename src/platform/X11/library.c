33febe216b0832b8946969569cf8128ad359f40c
Francisco Romano <fcalaisromano@gmail.com>
2024-11-21 22:30:56 +1100

    no need for absolute path when loading X11

--- src/platform/X11/library.c
+++ src/platform/X11/library.c
@@ -7,9 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#define X11_LIBRARY_PATHS(Name)\
-"/usr/lib/x86_64-linux-gnu/" Name,\
-"/snap/gnome-42-2204/176/usr/lib/x86_64-linux-gnu/" Name
 #define X11_LIBRARY_SYMBOL(Name)\
 X11.Name = dlsym(X11.lib, #Name); if (!X11.Name) { printf("ERROR: failed to load symbol '" #Name "'\n"); exit(EXIT_FAILURE); }
 // -------------------------------------------------------------------------------------------------------------------------- //
@@ -34,47 +31,40 @@ bool LoadLibraryX11()
 
     // iterate through all library paths
     const char* paths[] = {
-        X11_LIBRARY_PATHS("libX11.so.6.4.0"),
-        X11_LIBRARY_PATHS("libX11.so.6.4"),
+        "libX11.so.6.4.0",
+        "libX11.so.6.4",
         NULL
     };
     for (size_t i = 0; paths[i]; i++)
     {
         // try loading library and symbols from path
-        struct stat f;
-        if (stat(paths[i], &f) != 0) continue;
-        switch (f.st_mode & __S_IFMT)
-        {
-            case __S_IFLNK:
-            case __S_IFREG:
-                X11.lib = dlopen(paths[i], RTLD_LAZY);
-                if (X11.lib == NULL) continue;
-                X11_LIBRARY_SYMBOL(XInternAtom)
-                X11_LIBRARY_SYMBOL(XCreateColormap)
-                X11_LIBRARY_SYMBOL(XOpenDisplay)
-                X11_LIBRARY_SYMBOL(XChangeProperty)
-                X11_LIBRARY_SYMBOL(XCloseDisplay)
-                X11_LIBRARY_SYMBOL(XDefaultScreen)
-                X11_LIBRARY_SYMBOL(XDeleteContext)
-                X11_LIBRARY_SYMBOL(XDeleteProperty)
-                X11_LIBRARY_SYMBOL(XDestroyWindow)
-                X11_LIBRARY_SYMBOL(XFindContext)
-                X11_LIBRARY_SYMBOL(XFlush)
-                X11_LIBRARY_SYMBOL(XMapWindow)
-                X11_LIBRARY_SYMBOL(XPending)
-                X11_LIBRARY_SYMBOL(XNextEvent)
-                X11_LIBRARY_SYMBOL(XrmUniqueQuark)
-                X11_LIBRARY_SYMBOL(XSaveContext)
-                X11_LIBRARY_SYMBOL(XScreenCount)
-                X11_LIBRARY_SYMBOL(XUnmapWindow)
-                X11_LIBRARY_SYMBOL(XKeysymToKeycode)
-                X11_LIBRARY_SYMBOL(XDefaultScreenOfDisplay)
-                X11_LIBRARY_SYMBOL(XScreenOfDisplay)
-                X11_LIBRARY_SYMBOL(XSetWMProtocols)
-                X11_LIBRARY_SYMBOL(XCreateWindow)
-                X11_LIBRARY_SYMBOL(XDefaultRootWindow)
-                return true;
-        }
+        X11.lib = dlopen(paths[i], RTLD_LAZY);
+        if (X11.lib == NULL) continue;
+        X11_LIBRARY_SYMBOL(XInternAtom)
+        X11_LIBRARY_SYMBOL(XCreateColormap)
+        X11_LIBRARY_SYMBOL(XOpenDisplay)
+        X11_LIBRARY_SYMBOL(XChangeProperty)
+        X11_LIBRARY_SYMBOL(XCloseDisplay)
+        X11_LIBRARY_SYMBOL(XDefaultScreen)
+        X11_LIBRARY_SYMBOL(XDeleteContext)
+        X11_LIBRARY_SYMBOL(XDeleteProperty)
+        X11_LIBRARY_SYMBOL(XDestroyWindow)
+        X11_LIBRARY_SYMBOL(XFindContext)
+        X11_LIBRARY_SYMBOL(XFlush)
+        X11_LIBRARY_SYMBOL(XMapWindow)
+        X11_LIBRARY_SYMBOL(XPending)
+        X11_LIBRARY_SYMBOL(XNextEvent)
+        X11_LIBRARY_SYMBOL(XrmUniqueQuark)
+        X11_LIBRARY_SYMBOL(XSaveContext)
+        X11_LIBRARY_SYMBOL(XScreenCount)
+        X11_LIBRARY_SYMBOL(XUnmapWindow)
+        X11_LIBRARY_SYMBOL(XKeysymToKeycode)
+        X11_LIBRARY_SYMBOL(XDefaultScreenOfDisplay)
+        X11_LIBRARY_SYMBOL(XScreenOfDisplay)
+        X11_LIBRARY_SYMBOL(XSetWMProtocols)
+        X11_LIBRARY_SYMBOL(XCreateWindow)
+        X11_LIBRARY_SYMBOL(XDefaultRootWindow)
+        return true;
     }
     return false;
 }
