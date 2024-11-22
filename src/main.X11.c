4ae558abf5d53a98f897fa61aa1e4c32a28b9459
Francisco Romano <fcalaisromano@gmail.com>
2024-11-22 21:43:24 +1100

    updating example scenario for X11

--- src/main.X11.c
+++ src/main.X11.c
@@ -5,44 +5,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// https://xcb.freedesktop.org/opengl/
+// https://alexvia.com/post/002_initializing_opengl_on_x11/
+// https://github.com/gamedevtech/X11OpenGLWindow
+
 int main(int argc, char** argv)
 {
-    LibraryX11 x11;
-    if (!LoadLibraryX11(&x11))
+    if (!LoadLibraryX11())
     {
         printf("ERROR: failed to load X11 library\n");
         exit(EXIT_FAILURE);
     }
 
-    LibraryGLX glx;
-    if (!LoadLibraryGLX(&glx))
+    if (!LoadLibraryGLX())
     {
-        printf("ERROR: failed to load X11 library\n");
+        printf("ERROR: failed to load GLX library\n");
         exit(EXIT_FAILURE);
     }
 
     WindowContextX11 ctx;
-    if (!CreateWindowContextX11(&x11, &ctx))
+    if (!CreateWindowContextX11(&ctx))
     {
         printf("ERROR: failed to create X11 window context\n");
         exit(EXIT_FAILURE);
     }
 
     WindowX11 win;
-    if (!CreateWindowX11(&x11, &ctx, 800, 600, &win))
+    if (!CreateWindowX11(&ctx, 800, 600, &win))
     {
         printf("ERROR: failed to create X11 window\n");
         exit(EXIT_FAILURE);
     }
 
-    while (ReadWindowEventsX11(&x11, &ctx))
+    while (ReadWindowEventsX11(&ctx))
     {
         sleep(0);
     }
 
-    DestroyWindowX11(&x11, &ctx, &win);
-    DestroyWindowContextX11(&x11, &ctx);
-    FreeLibraryGLX(&glx);
-    FreeLibraryX11(&x11);
+    DestroyWindowX11(&ctx, &win);
+    DestroyWindowContextX11(&ctx);
+    FreeLibraryGLX();
+    FreeLibraryX11();
     return 0;
 }
