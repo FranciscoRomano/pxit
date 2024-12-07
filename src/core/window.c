2bcc0e830647c3d56c98e97ae3105a2f97fd0905
Francisco Romano <fcalaisromano@gmail.com>
2024-12-07 23:28:56 +1100

    adding platform specific implementations to window API

--- src/core/window.c
+++ src/core/window.c
@@ -20,13 +20,34 @@ bool RestoreWindow(Window window)                               { INVOKE_FUNCTIO
 bool ShowWindow(Window window)                                  { INVOKE_FUNCTION(ShowWindow)                }
 bool SizeWindow(Window window, uint32_t width, uint32_t height) { INVOKE_FUNCTION(SizeWindow, width, height) }
 // -------------------------------------------------------------------------------------------------------------------------- //
+#if IS_PLATFORM_LINUX
+bool _CreateWindowX11(const WindowCreateInfo* pCreateInfo, Window* pWindow);
+#elif IS_PLATFORM_WINDOWS
+bool _CreateWindowWin32(const WindowCreateInfo* pCreateInfo, Window* pWindow);
+#endif
 bool CreateWindow(const WindowCreateInfo* pCreateInfo, Window* pWindow)
 {
+    #if IS_PLATFORM_LINUX
+    if (_CreateWindowX11(pCreateInfo, pWindow)) return true;
+    #elif IS_PLATFORM_WINDOWS
+    if (_CreateWindowWin32(pCreateInfo, pWindow)) return true;
+    #endif
     return false;
 }
 // -------------------------------------------------------------------------------------------------------------------------- //
+#if IS_PLATFORM_LINUX
+bool _ReadWindowEventsX11();
+#elif IS_PLATFORM_WINDOWS
+bool _ReadWindowEventsWin32();
+#endif
 bool ReadWindowEvents()
 {
-    return false;
+    bool result = false;
+    #if IS_PLATFORM_LINUX
+    result |= _ReadWindowEventsX11();
+    #elif IS_PLATFORM_WINDOWS
+    result |= _ReadWindowEventsWin32();
+    #endif
+    return result;
 }
 // -------------------------------------------------------------------------------------------------------------------------- //
