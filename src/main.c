200f03198f63ca4b19874821d838e39ac4d86169
FranciscoRomano <58254306+FranciscoRomano@users.noreply.github.com>
2024-12-14 21:52:05 +1100

    resizing viewport on window size

--- src/main.c
+++ src/main.c
@@ -22,13 +22,19 @@ void on_window_paint(Window window)
     Draw_GLES3();
 }
 
+void on_window_size(Window window, uint32_t width, uint32_t height)
+{
+    GLES20.glViewport(0, 0, width, height);
+}
+
 int main(int argc, char** argv)
 {
     WindowCallbacks callbacks;
     memset(&callbacks, 0, sizeof(WindowCallbacks));
+    callbacks.OnWindowClose  = on_window_close;
     callbacks.OnWindowCreate = on_window_create;
     callbacks.OnWindowPaint  = on_window_paint;
-    callbacks.OnWindowClose  = on_window_close;
+    callbacks.OnWindowSize   = on_window_size;
 
     WindowCreateInfo create_info;
     create_info.Left       = 40;
