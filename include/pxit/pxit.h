74145448513007adb71d4ea0a90ee5931f607399
FranciscoRomano <58254306+FranciscoRomano@users.noreply.github.com>
2024-12-21 11:40:32 +1100

    updating master header file to include OpenGL ES 3.2 and core window api

--- include/pxit/pxit.h
+++ include/pxit/pxit.h
@@ -5,27 +5,17 @@
 #ifndef __pxit_pxit_h__
 #define __pxit_pxit_h__
 // ---------------------------------------------------------------------------------------------- //
-// IS_PLATFORM_LINUX - Used to detect any GNU/Linux Operating System
-#if defined(linux) || defined(__linux) || defined(__linux__)
-  #define IS_PLATFORM_LINUX 1
-#else
-  #define IS_PLATFORM_LINUX 0
-#endif
-// IS_PLATFORM_WINDOWS - Used to detect any Microsoft Windows Operating System
-#if defined(_WIN32) || defined(_WIN64)
-  #define IS_PLATFORM_WINDOWS 1
-#else
-  #define IS_PLATFORM_WINDOWS 0
-#endif
-// PLATFORM_CALL - Placed after the return type in function declarations.
-//                 Useful for MSVC-style calling convention syntax.
-#ifndef PLATFORM_CALL
-  #if IS_PLATFORM_WINDOWS
-    #define PLATFORM_CALL __stdcall
-  #else
-    #define PLATFORM_CALL
-  #endif
-#endif
+
+#ifndef NULL
+#define NULL (void*)0
+#endif//NULL
+
+#define GL_APICALL
+#define GL_APIENTRY
+#include <GLES3/gl32.h>
+
+#include <pxit/core/window.h>
+
 // ---------------------------------------------------------------------------------------------- //
 #endif//__pxit_pxit_h__
 // ---------------------------------------------------------------------------------------------- //
